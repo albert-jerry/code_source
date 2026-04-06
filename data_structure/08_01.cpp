6b85c485566bb34925779df98e1591f4b1480401
albert-jerry <zhygbest@163.com>
2026-04-06 14:45:30 +0800

    Up_08

--- data_structure/08_01.cpp
+++ data_structure/08_01.cpp
@@ -1,57 +1,53 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+#include <vector>
+#include <string>
 using namespace std;
 
-struct tree {
-    int value, l;
-    tree *wh[100];
-} a[100000];
+struct tree{
+    int value;
+    vector<int> c;
+}a[100000];
 
-inline string find(int u, int z, int v, string s) {
-    string s1 = "";
-    int l = 0, k = z;
-    while (k) {
-        l++;
-        k /= 10;
-    }
-    k = z;
-    for (int i = 0; i < l; i++) {
-        s1 += (k%10) + '0';
-    }
-    s += s1;
-    s += ' ';
-    if (z == v) {
+string DFS(int from, int x, int y, string s) {
+    if (x == y) {
         return s;
     }
-    int f = 0;
-    for (int i = 0; i < a[z].l; i++) {
-        if (a[z].wh[i]->value != u) {
-            f++;
-            return find(a[z].value, a[z].wh[i]->value, v, s);
+    for (int i = 0; i < a[x].c.size(); i++) {
+        int next = a[x].c[i];
+        if (next == from) {
+            continue;
+        }
+        DFS(a[i].value, a[next].value, y, s);
+        if (s != "error") {
+            s = to_string(a[next].value + 1) + " " + s;
+            return s;
         }
     }
-    if (f == 0) {
-        return "-1";
-    }
+    return "error";
+}
+
+void road(int x, int y) {
+    string s = "";
+    s = DFS(-1, x, y, s);
+    cout << s << endl;
 }
 
 int main() {
     int n;
     scanf("%d", &n);
-    for  (int i = 1; i <= n; i++) {
-        a[i].value = i;
-    }
     for (int i = 0; i < n - 1; i++) {
-        int x, y;
-        scanf("%d%d", &x, &y);
-        a[x].wh[a[x].l] = &a[y];
-        a[y].wh[a[y].l] = &a[x];
-        a[x].l++, a[y].l++;
+        a[i].value = i;
+        int b, c;
+        scanf("%d%d", &b, &c);
+        b--, c--;
+        a[b].c.push_back(c);
+        a[c].c.push_back(b);
     }
     int u, v;
     scanf("%d%d", &u, &v);
-    string s = "";
-    cout << find(u, u, v, s);
+    u--, v--;
+    road(u, v);
     return 0;
 }
